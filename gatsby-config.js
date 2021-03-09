992185f9c45a7c2a52f7baa565e6117aabdd5878
phamyy <45474071+phamyy@users.noreply.github.com>
2021-03-09 01:23:09 -0800

    starting personal website project

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,12 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
